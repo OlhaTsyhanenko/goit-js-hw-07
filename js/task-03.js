0de0f9aec2f2faf525b0d507eca3330f0527a6aa
Olha Tsyhanenko <olya.tsyganenko@gmail.com>
2021-07-07 22:35:07 +0300

    update task-03.js

---
+++ js/task-03.js
@@ -0,0 +1,33 @@
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+const ulEl = document.querySelector('ul#gallery');
+
+const addItemImages = (images) => {
+  return `
+    <li class = item><img src = ${images.url} alt = ${images.alt} class = item__img></li>
+    `;
+};
+
+const add = images.map(addItemImages).join('');
+console.log(add);
+
+ulEl.insertAdjacentHTML('afterbegin', add);
+
+
+
+
+
+
