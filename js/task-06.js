244372db6708687ba14097b495092270507a8f8a
Olha Tsyhanenko <olya.tsyganenko@gmail.com>
2021-07-15 00:33:41 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -6,10 +6,8 @@ function onInputBlur(event) {
     console.log(Number(event.currentTarget.value.length));
     if (event.currentTarget.value.length === Number(input.dataset.length)) {
         input.classList.add('valid');
-    } if (event.currentTarget.value.length !== Number(input.dataset.length)) {
+    }
+    else {
         input.classList.add('invalid');
-    } if (event.currentTarget.value.length === 0) {
-        input.classList.remove('valid');
-        input.classList.remove('invalid');
-    }  
-}
+}}
+        
