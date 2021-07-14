f879bd1a623e89245da88393ae2ad7bf0fd754cf
Olha Tsyhanenko <olya.tsyganenko@gmail.com>
2021-07-15 00:21:36 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -4,9 +4,8 @@ const output = document.querySelector('span#name-output');
 input.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
-    output.textContent = event.currentTarget.value;
-    if (output.textContent === "") {
-        output.textContent = 'незнакомец';
-    }
-    //console.log(event.currentTarget.value);
+    if (event.currentTarget.value.trim()) {
+        output.textContent = event.currentTarget.value;
+    } else output.textContent = 'незнакомец';
+    
 }
