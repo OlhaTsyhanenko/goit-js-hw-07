e0ca75874c0c94a9abd21bab7c01e7d12968a81f
Olha Tsyhanenko <olya.tsyganenko@gmail.com>
2021-07-15 00:03:03 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,23 +1,14 @@
 const ulEl = document.querySelector('ul#categories');
-//console.log(ulEl);
-
 const itemCategoryEl = ulEl.querySelectorAll('.item');
 console.log(`В списке ${itemCategoryEl.length} категории.`);
 
-const countFirstCategoryEl = itemCategoryEl[0].querySelectorAll('li');
-//console.log(itemCategoryEl[0]);
-const titleFirstCategoryEl = itemCategoryEl[0].querySelector('h2');
-console.log(titleFirstCategoryEl.textContent);
-console.log(`Количество элементов: ${countFirstCategoryEl.length}`);
+function countElements(itemCategoryEl) {
+    
+    for (let i = 0; i < itemCategoryEl.length; i += 1){
+       console.log(itemCategoryEl[i].querySelector('h2').textContent);
+       console.log(`Количество элементов: ${itemCategoryEl[i].querySelectorAll('li').length}`);
+    }
+    
+    }
 
-const countSecondCategoryEl = itemCategoryEl[1].querySelectorAll('li');
-//console.log(itemCategoryEl[1]);
-const titleSecondCategoryEl = itemCategoryEl[1].querySelector('h2');
-console.log(titleSecondCategoryEl.textContent);
-console.log(`Количество элементов: ${countSecondCategoryEl.length}`);
-
-const countThirdCategoryEl = itemCategoryEl[2].querySelectorAll('li');
-const titleThirdCategoryEl = itemCategoryEl[2].querySelector('h2');
-console.log(titleThirdCategoryEl.textContent);
-//console.log(itemCategoryEl[2]);
-console.log(`Количество элементов: ${countThirdCategoryEl.length}`);
+console.log(countElements(itemCategoryEl));
