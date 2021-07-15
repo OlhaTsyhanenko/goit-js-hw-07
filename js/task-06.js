a07c628252bc0e46ee1c03d54f8ebf2c4036c847
Olha Tsyhanenko <olya.tsyganenko@gmail.com>
2021-07-15 14:51:10 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -6,8 +6,10 @@ function onInputBlur(event) {
     console.log(Number(event.currentTarget.value.length));
     if (event.currentTarget.value.length === Number(input.dataset.length)) {
         input.classList.add('valid');
+        input.classList.remove('invalid');
     }
     else {
         input.classList.add('invalid');
+        input.classList.remove('valid');
 }}
         
