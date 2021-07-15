e758bac92937555f3091c2a865f7e47fd8aeb5a9
Olha Tsyhanenko <olya.tsyganenko@gmail.com>
2021-07-15 14:42:14 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 const input = document.querySelector("input#font-size-control");
 const span = document.querySelector('span#text');
 
-input.addEventListener('click', onInputClick);
+input.addEventListener('input', onInputClick);
 
 function onInputClick(event) {
     span.style.fontSize = event.currentTarget.value + 'px';
